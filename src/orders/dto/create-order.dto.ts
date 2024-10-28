4ec6a9e0cfc3981f42986f31efb585955673686f
ramiro <ramiro.sarasola@gmail.com>
2024-10-28 10:50:07 -0300

    create order items model

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,29 +1,42 @@
 import {
+  ArrayMinSize,
+  IsArray,
   IsBoolean,
   IsEnum,
   IsNumber,
   IsOptional,
   IsPositive,
+  ValidateNested,
 } from 'class-validator';
 import { OrderStatusList } from '../enum/order.enum';
 import { OrdersStatus } from '@prisma/client';
+import { OrderItemDto } from './order-item.dto';
+import { Type } from 'class-transformer';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  public totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  public totalItems: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Order status must be one of: ${OrderStatusList.join(', ')}`,
-  })
-  @IsOptional()
-  public status: OrdersStatus = OrdersStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  public paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  public items: OrderItemDto[];
 }
+
+// export class CreateOrderDto {
+//   @IsNumber()
+//   @IsPositive()
+//   public totalAmount: number;
+
+//   @IsNumber()
+//   @IsPositive()
+//   public totalItems: number;
+
+//   @IsEnum(OrderStatusList, {
+//     message: `Order status must be one of: ${OrderStatusList.join(', ')}`,
+//   })
+//   @IsOptional()
+//   public status: OrdersStatus = OrdersStatus.PENDING;
+
+//   @IsBoolean()
+//   @IsOptional()
+//   public paid: boolean = false;
+// }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -17,7 +17,12 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return this.order.create({ data: createOrderDto });
+    return {
+      service: 'Orders Microservice',
+      createOrderDto: createOrderDto,
+    };
+
+    // return this.order.create({ data: createOrderDto });
   }
 
   async findAll(orderPaginationDto: OrdersPaginatioDto) {
